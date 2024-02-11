e462aee911ad490fe40f7f346d69daa33662e8fe
Kevin Cantrell <kevin@cropwatch.io>
2024-02-11 11:49:32 +0900

    more improvements to data page

--- src/routes/app/locations/[location_name]/[sensor_eui]/+page.svelte
+++ src/routes/app/locations/[location_name]/[sensor_eui]/+page.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
-	import { subSeconds } from 'date-fns';
 	import CwBadge from '$lib/components/badge/CWBadge.svelte';
-	import RadarChart from '$lib/components/charts/highcharts/highcharts-radarchart/RadarChart.svelte';
-	import CWStatCard from '$lib/components/stat-card/CWStatCard.svelte';
 	import { Avatar, Badge, Button, Card, Duration, Header, Tabs } from 'svelte-ux';
-	import LineChart from '$lib/components/charts/highcharts/lineChart/LineChart.svelte';
 	import { mdiArrowLeft } from '@mdi/js';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import Details from './details/Details.svelte';
+	import History from './history/History.svelte';
+	import Rules from './rules/Rules.svelte';
+	import Notifications from './notifications/Notifications.svelte';
+	import Settings from './settings/Settings.svelte';
+	import Permissions from './permissions/Permissions.svelte';
 
 	let userAvailableTabs = [
 		{ label: 'Dashboard', value: 1 },
 		{ label: 'History', value: 2 },
-		{ label: 'Settings', value: 3 },
+		{ label: 'Rules', value: 3 },
 		{ label: 'Notifications', value: 4 },
-		{ label: 'Permissions', value: 5 }
+		{ label: 'Settings', value: 5 },
+		{ label: 'Permissions', value: 6 }
 	];
 	let currentTab: number = 1;
 </script>
 
 <h1 class="text-4xl font-semibold text-slate-700 mb-4">
-	<Button size="lg" icon={mdiArrowLeft} on:click={() => goto(`/app/locations/${$page.params.location_name}`)} />
+	<Button
+		size="lg"
+		icon={mdiArrowLeft}
+		on:click={() => goto(`/app/locations/${$page.params.location_name}`)}
+	/>
 	CW-SS-THEPNPK
 </h1>
 
@@ -44,53 +51,19 @@
 </div>
 
 <Tabs options={userAvailableTabs} bind:value={currentTab}>
-	<!-- <svelte:fragment slot="content" let:value>
-		Page {value}
-	</svelte:fragment> -->
+	<svelte:fragment slot="content" let:value>
+		{#if value === 1}
+			<Details />
+		{:else if value === 2}
+			<History />
+		{:else if value === 3}
+			<Rules />
+		{:else if value === 4}
+			<Notifications />
+			{:else if value === 5}
+			<Settings />
+			{:else if value === 6}
+			<Permissions />
+		{/if}
+	</svelte:fragment>
 </Tabs>
-
-<div class="grid grid-cols-2 mt-10 gap-4 mb-2">
-	<CWStatCard title="Current Temperature" />
-	<CWStatCard title="Current Moisture" />
-</div>
-
-<div class="grid grid-cols-2 gap-2 mb-2">
-	<Card>
-		<Header title={'Soil Temperature'} slot="header" class="gap-0">
-			<div slot="subheading">
-				Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
-			</div>
-		</Header>
-		<LineChart />
-	</Card>
-	<Card>
-		<Header title={'Soil Moisture'} slot="header" class="gap-0">
-			<div slot="subheading">
-				Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
-			</div>
-		</Header>
-		<div class="flex flex-row">
-			<LineChart />
-		</div>
-	</Card>
-</div>
-
-<div class="grid grid-cols-3 mt-2 gap-4 mb-2">
-	<CWStatCard title="Soil N(nitrogen)" />
-	<CWStatCard title="Soil P(phosphorus)" />
-	<CWStatCard title="Soil K(potassium)" />
-</div>
-
-<div class="grid grid-cols-2 mt-2 gap-4 mb-2">
-	<CWStatCard title="Soil pH" />
-	<CWStatCard title="Soil EC" />
-</div>
-
-<Card>
-	<Header title={'Soil Temperature'} slot="header" class="gap-0">
-		<div slot="subheading">
-			Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
-		</div>
-	</Header>
-	<RadarChart N={10} P={33} K={50} EC={150} PH={5.7} />
-</Card>
---
+++ src/routes/app/locations/[location_name]/[sensor_eui]/details/Details.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { subSeconds } from 'date-fns';
+	import RadarChart from '$lib/components/charts/highcharts/highcharts-radarchart/RadarChart.svelte';
+	import CWStatCard from '$lib/components/stat-card/CWStatCard.svelte';
+	import LineChart from '$lib/components/charts/highcharts/lineChart/LineChart.svelte';
+	import { Card, Duration, Header } from 'svelte-ux';
+
+</script>
+
+<div class="grid grid-cols-2 mt-10 gap-4 mb-2">
+	<CWStatCard title="Temperature" />
+	<CWStatCard title="Moisture" />
+</div>
+
+<div class="grid grid-cols-2 gap-2 mb-2">
+	<Card>
+		<Header title={'Soil Temperature'} slot="header" class="gap-0">
+			<div slot="subheading">
+				Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
+			</div>
+		</Header>
+		<LineChart />
+	</Card>
+	<Card>
+		<Header title={'Soil Moisture'} slot="header" class="gap-0">
+			<div slot="subheading">
+				Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
+			</div>
+		</Header>
+		<div class="flex flex-row">
+			<LineChart />
+		</div>
+	</Card>
+</div>
+
+<div class="grid grid-cols-3 mt-2 gap-4 mb-2">
+	<CWStatCard title="Soil N" />
+	<CWStatCard title="Soil P" />
+	<CWStatCard title="Soil K" />
+</div>
+
+<div class="grid grid-cols-2 mt-2 gap-4 mb-2">
+	<CWStatCard title="Soil pH" />
+	<CWStatCard title="Soil EC" />
+</div>
+
+<Card>
+	<Header title={'Soil Temperature'} slot="header" class="gap-0">
+		<div slot="subheading">
+			Last Update <Duration start={subSeconds(new Date(), 0)} totalUnits={1} /> ago
+		</div>
+	</Header>
+	<RadarChart N={10} P={33} K={50} EC={150} PH={5.7} />
+</Card>
