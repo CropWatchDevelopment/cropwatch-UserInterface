c37003a62b6183610f9da2221f75808b4a97eb75
Kevin Cantrell <kevin@cropwatch.io>
2024-02-18 00:44:52 +0900

    adding settings pages

--- src/routes/app/locations/[location_id]/[sensor_eui]/settings/Settings.svelte
+++ src/routes/app/locations/[location_id]/[sensor_eui]/settings/Settings.svelte
@@ -1,1 +1,37 @@
-Settings
+<script lang="ts">
+	import { mdiFloppy, mdiSpeaker, mdiTimer } from '@mdi/js';
+	import { Button, NumberStepper, SelectField } from 'svelte-ux';
+
+	let frequency: number = 10;
+</script>
+
+<h1 class="text-lg font-bold">Sensor Settings</h1>
+
+<div class="flex flex-col gap-2">
+	<h1>Send Frequency</h1>
+	<NumberStepper min={10} max={1440} bind:value={frequency} />
+</div>
+
+<div class="grid grid-flow-row grid-cols-3 gap-2 mt-4">
+	<div>
+		<h1>Nitrogen Calibration</h1>
+		<NumberStepper min={10} max={1440} bind:value={frequency} />
+	</div>
+
+	<div>
+		<h1>Phosphorus Calibration</h1>
+		<NumberStepper min={10} max={1440} bind:value={frequency} />
+	</div>
+
+	<div>
+		<h1>Potassium Calibration</h1>
+		<NumberStepper min={10} max={1440} bind:value={frequency} />
+	</div>
+
+</div>
+
+<div class="my-4">
+    <h1>Send ID Signal to device (only some models support this feature)</h1>
+    <Button variant="fill-light" color={'accent'} icon={mdiSpeaker}>Send</Button>
+</div>
+<Button variant="fill-light" color={'accent'} icon={mdiFloppy}>Save</Button>
